669d6b059b327e86bf52be80ee7059e4f7ea93da
Soledium <nescobar3@hotmail.com>
2022-07-05 13:27:52 -0400

    agregando desafio evaluado top_100

---
+++ desafio_top_100.sql
@@ -0,0 +1,30 @@
+-- paso 1 
+CREATE DATABASE peliculas;
+-- paso 2
+CREATE TABLE pelicula (id BIGINT, pelicula TEXT, año_estreno TEXT, director NAME, PRIMARY KEY (id))
+-- paso 3
+CREATE TABLE reparto (id INTEGER, nombre NAME, FOREIGN KEY )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
